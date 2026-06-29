b581718926e82f87313c234df98735929d157d08
Ritika <ritikavr2004@gmail.com>
2026-06-29 15:39:11 +0530

    Change S3 backend region from us-east-1 to ap-south-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-statelock-file"
     key          = "Nodeexporter/terraform.tfstate"
-    region       = "us-east-1"
+    region       = "ap-south-1"
     use_lockfile = true
   }
 }
